06735e0be9a2df92d2ff55725d9f58b392103d2a
Penny Wood <github@ninjateaparty.com>
2019-06-15 18:53:18 +0800

    Tweaks

--- setup.py
+++ setup.py
@@ -13,16 +13,20 @@ setup(
     description="A python interface to the iZone airconditioner controller",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
+    keywords=['iZone', 'IoT', ],
     url="https://github.com/Swamp-Ig/pizone",
+    python_requires='~=3.5',
     install_requires=['aiohttp>=3.4', 'netifaces'],
     tests_require=['aiounittest'],
     packages=find_packages(exclude=['tests', 'tests.*']),
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
         "License :: OSI Approved :: "
             "GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
+        "Topic :: Home Automation",
+        "Topic :: System :: Hardware"
     ],
 )
