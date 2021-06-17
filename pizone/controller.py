2f11b56d62605d14c9db950314ce8aeca438fe7b
SgtBatten <sgt@batten.net.au>
2021-06-18 09:49:51 +1000

    add 4 speed fan as unknown
    izone/airstream have advised that it is normal for FanAuto to report "unknown" when 4-speed is selected in the Auto Fan configuration on an iZone controller.

--- pizone/controller.py
+++ pizone/controller.py
@@ -33,6 +33,7 @@ class Controller:
         LOW = 'low'
         MED = 'med'
         HIGH = 'high'
+        BOOST = 'top'
         AUTO = 'auto'
 
     DictValue = Union[str, int, float]
@@ -46,6 +47,7 @@ class Controller:
 
     _VALID_FAN_MODES = {
         'disabled': [Fan.LOW, Fan.MED, Fan.HIGH],
+        'unknown': [Fan.LOW, Fan.MED, Fan.HIGH, Fan.BOOST, Fan.AUTO],
         '3-speed': [Fan.LOW, Fan.MED, Fan.HIGH, Fan.AUTO],
         '2-speed': [Fan.LOW, Fan.HIGH, Fan.AUTO],
         'var-speed': [Fan.LOW, Fan.MED, Fan.HIGH, Fan.AUTO],
