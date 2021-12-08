6e4e04f4ff2b33191fcb76cc50ff5dcc16c3ae2d
Swamp-Ig <github@ninjateaparty.com>
2021-12-08 17:08:20 +0800

    Bump version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="python-izone",
-    version="1.1.8",
+    version="1.2.0",
     author="Penny Wood",
     author_email="pypl@ninjateaparty.com",
     description="A python interface to the iZone airconditioner controller",
